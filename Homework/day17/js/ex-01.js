0492522065d79948a90d029c4d4676aee9f45869
= <chuthanh.hung@petronas.com>
2024-01-26 21:05:16 +0700

    day17

--- Homework/day17/js/ex-01.js
+++ Homework/day17/js/ex-01.js
@@ -1,17 +1,18 @@
 let btn = document.getElementById("btn");
 let show = document.getElementById("showBtn");
-btn.addEventListener("click", () => {
-  let numbersOfKm = prompt("Nhập số km:");
 
-  let taxiFee = 0;
-  if (numbersOfKm <= 1) {
-    taxiFee = 15000;
-  } else if (numbersOfKm > 1 && numbersOfKm <= 5) {
-    taxiFee = 13500 * numbersOfKm;
-  } else if (numbersOfKm > 5 && numbersOfKm <= 120) {
-    taxiFee = 11000 * numbersOfKm;
-  } else {
-    taxiFee = 11000 * numbersOfKm * 0.9;
-  }
-  show.innerText = taxiFee;
+btn.addEventListener("click", () => {
+    let numbersOfKm = prompt("Nhập số km:");
+
+    let taxiFee = 0;
+    if (numbersOfKm <= 1) {
+        taxiFee = 15000;
+    } else if (numbersOfKm > 1 && numbersOfKm <= 5) {
+        taxiFee = 13500 * numbersOfKm;
+    } else if (numbersOfKm > 5 && numbersOfKm <= 120) {
+        taxiFee = 11000 * numbersOfKm;
+    } else {
+        taxiFee = 11000 * numbersOfKm * 0.9;
+    }
+    show.innerText = `${numbersOfKm}Km => ${taxiFee} vnđ`;
 });
